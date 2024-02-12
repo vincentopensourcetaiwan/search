97f92ab326ae6461d8e71bf352992050f054ff08
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-12 15:49:58 +0800

    setup search form

--- lib/search_web/live/home.ex
+++ lib/search_web/live/home.ex
@@ -4,10 +4,20 @@ defmodule SearchWeb.Home do
   def render(assigns) do
     ~H"""
     <h1>Welcome to Phoenix!</h1>
+    <form phx-submit="submit">
+      <input type="text" name="name" value={@name} />
+      <button type="submit">search</button>
+    </form>
     """
   end
 
   def mount(_params, _session, socket) do
+    socket = assign(socket, name: "")
     {:ok, socket}
+  end
+
+  def handle_event("submit", %{"name" => name}, socket) do
+    IO.inspect(name)
+    {:noreply, socket}
   end
 end
