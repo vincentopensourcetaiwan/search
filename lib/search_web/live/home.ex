a59c2428f9555cbe1fc4b68f314f5fab9e1ca39b
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-12 16:11:17 +0800

    show all tag

--- lib/search_web/live/home.ex
+++ lib/search_web/live/home.ex
@@ -1,6 +1,9 @@
 defmodule SearchWeb.Home do
   use SearchWeb, :live_view
 
+  alias Search.Repo
+  alias Search.Contents.Tag
+
   def render(assigns) do
     ~H"""
     <h1>Welcome to Phoenix!</h1>
@@ -8,16 +11,20 @@ defmodule SearchWeb.Home do
       <input type="text" name="name" value={@name} />
       <button type="submit">search</button>
     </form>
+    <%= for tag <- @result do %>
+      <p><%= tag.name %></p>
+    <% end %>
     """
   end
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, name: "")
+    socket = assign(socket, name: "", result: [])
     {:ok, socket}
   end
 
   def handle_event("submit", %{"name" => name}, socket) do
-    IO.inspect(name)
+    result = Repo.all(Tag)
+    socket = assign(socket, name: name, result: result)
     {:noreply, socket}
   end
 end
