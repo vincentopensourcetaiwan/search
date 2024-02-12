54ba31bb9f740df72bdc4e27a3dd8fe8df81d429
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-12 15:57:42 +0800

    seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Search.Repo
+alias Search.Contents.Tag
+
+Repo.insert!(%Tag{name: "elixir"})
+Repo.insert!(%Tag{name: "phoenix"})
+Repo.insert!(%Tag{name: "ecto"})
